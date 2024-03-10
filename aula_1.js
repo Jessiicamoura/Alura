17d59b18162c1ea4a578343f41cd28c5d12f73b1
Jessiicamoura <162493569+Jessiicamoura@users.noreply.github.com>
2024-03-09 22:41:16 -0300

    Add files via upload

---
+++ aula_1.js
@@ -0,0 +1,32 @@
+alert("Bem vindo ao jogo do número secreto");
+let numeroSecreto = parseInt(Math.random() *100 +1);
+console.log(numeroSecreto);
+let chute;
+let tentativas = 1;
+
+// enquanto chute não for igual ao n.s
+while (chute != numeroSecreto) {
+    chute = prompt("Escolha um número entre 1 a 100");
+    // se o chute for igual ao dia secreto
+    if (chute == numeroSecreto) {
+        break;
+    } else {
+        if(chute > numeroSecreto) {
+            alert(`o número secreto é menor que ${chute}`);
+        } else {
+            alert (`o número secreto é maior que ${chute}`);
+        }   
+        //tentativas = tentativas + 1;
+        tentativas++;
+    }
+}
+
+let palavraTentativa = tentativas > 1 ? "tentativas" : "tentativa"
+alert (`Isso ai! você acertou o número secreto ${numeroSecreto} com ${tentativas} ${palavraTentativa}.`);
+
+// if (tentativas > 1 ){
+//    alert (`Isso ai! você acertou o número secreto ${numeroSecreto} com ${tentativas} tentativas.`);
+// } else {
+//   alert (`Isso ai! você acertou o número secreto ${numeroSecreto} com ${tentativas} tentativa.`);
+// }
+
